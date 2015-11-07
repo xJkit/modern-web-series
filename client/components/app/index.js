15191b3b0e037e60f5d9b198b4aa87471b38c45d
Phil Parsons <phil.m.parsons@gmail.com>
2015-11-07 09:38:35 +0000

    End of webpack article

---
+++ client/components/app/index.js
@@ -0,0 +1,8 @@
+import React, {Component} from 'react';
+import {} from './style.less';
+
+export default class App extends Component {
+  render() {
+    return <div>Hello world from a React component</div>;
+  }
+}
