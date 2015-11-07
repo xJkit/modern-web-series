ddf4e18ea02e0beb058a7c9b2a5bf937f5cf90bc
Phil Parsons <phil.m.parsons@gmail.com>
2015-11-07 10:45:41 +0000

    End of express article

--- client/components/app/index.js
+++ client/components/app/index.js
@@ -1,8 +1,10 @@
 import React, {Component} from 'react';
 import {} from './style.less';
 
-export default class App extends Component {
+class App extends Component {
   render() {
     return <div>Hello world from a React component</div>;
   }
 }
+
+export default App;
